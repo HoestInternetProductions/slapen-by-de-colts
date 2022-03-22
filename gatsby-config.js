1d5047f5f1ca0d9e7f9ca3caea0813572db48b55
Jelle de Jong <jelle@twijg-it.nl>
2022-03-22 11:05:24 +0100

    Fix voor Google Analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,23 @@
 /** @type {import('gatsby').GatsbyConfig} */
 module.exports = {
   siteMetadata: {
-    title: ``,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `Slapen by de Colts - Bed and breakfast in Soest`,
+    siteUrl: `https://slapenbydecolts.nl`,
   },
   plugins: [
     "gatsby-plugin-postcss",
     {
-      resolve: "gatsby-plugin-google-analytics",
+      resolve: `gatsby-plugin-google-gtag`,
       options: {
-        trackingId: "G-QDRG091VZC",
+        trackingIds: ["G-QDRG091VZC"],
+        gtagConfig: {
+          optimize_id: "OPT_CONTAINER_ID",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        pluginConfig: {
+          respectDNT: true,
+        },
       },
     },
     "gatsby-plugin-image",
